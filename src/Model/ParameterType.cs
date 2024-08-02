4d338d2197b64c92fce1f041792a0a8c5b29d979
constantin_k <strongtigerman@gmail.com>
2024-08-02 12:21:30 +0300

    parameters fix

---
+++ src/Model/ParameterType.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Morph.Server.Sdk.Model
+{
+    public enum ParameterType
+    {
+        [Description("Text or number")]
+        Text,
+        [Description("File name")]
+        FilePath,
+        [Description("Date")]
+        Date,
+        [Description("Calculated")]
+        Calculated,
+        [Description("Folder path")]
+        FolderPath,
+        [Description("Checkbox")]
+        Checkbox,
+        [Description("Fixed list")]
+        FixedList,
+        [Description("Multiple choice")]
+        MultipleChoice
+
+    }
+
+
+    
+
+}
