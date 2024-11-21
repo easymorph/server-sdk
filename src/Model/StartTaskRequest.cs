07c3c87fc097ed458aee8dc9ca23afa737499a78
Alexander Lysak <alexander.lysak@gmail.com>
2024-11-21 15:18:04 +0100

    Allow specifying upload Id for Task

--- src/Model/StartTaskRequest.cs
+++ src/Model/StartTaskRequest.cs
@@ -6,6 +6,9 @@ namespace Morph.Server.Sdk.Model
     public sealed class StartTaskRequest
     {
         public Guid TaskId { get; set; }
+        
+        public string UploadContextId { get; set; }
+
         public IEnumerable<ParameterNameValue> TaskParameters { get; set; }
 
         public StartTaskRequest(Guid taskId)
