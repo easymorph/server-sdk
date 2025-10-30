f2d43ab0d5b6e9598ce1b9c821776316bfaaeb28
ynborokh <y.borokh@gmail.com>
2025-10-30 13:43:55 +0200

    Add increment shared memory value API

---
+++ src/Dto/SharedMemory/IncrementSharedMemoryValueDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Morph.Server.Sdk.Dto.SharedMemory
+{
+    [DataContract]
+    internal class IncrementSharedMemoryValueDto
+    {
+        public static class BehaviorCodes
+        {
+            public const string Throw = "Throw";
+            public const string UseDefault = "UseDefault";
+        }
+
+        [DataMember(Name = "key")]
+        public string Key { get; set; }
+
+        [DataMember(Name = "value")]
+        public decimal Value { get; set; }
+
+        [DataMember(Name = "missingKeyBehavior")]
+        public string MissingKeyBehavior { get; set; }
+    }
+}
---
+++ src/Model/SharedMemory/MissingKeyBehavior.cs
@@ -0,0 +1,18 @@
+﻿namespace Morph.Server.Sdk.Model.SharedMemory
+{
+    /// <summary>
+    /// What to do if value does not exist
+    /// </summary>
+    public enum MissingKeyBehavior
+    {
+        /// <summary>
+        /// Throws an exception if key does not exist
+        /// </summary>
+        Throw,
+
+        /// <summary>
+        /// Uses default type value instead, if key does not exist
+        /// </summary>
+        UseDefault,
+    }
+}
